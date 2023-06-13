fd47cb173b919aa9dd6948646feb1ea825dd4b3e
woorek <lkjwj95@gmail.com>
2023-06-13 18:16:58 +0900

    little change in currency.py

--- airflow/dags/currency.py
+++ airflow/dags/currency.py
@@ -1,34 +1,37 @@
 from airflow import DAG
-from datetime import datetime, timedelta
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
+from datetime import datetime
+import pytz, pendulum
+
+KST = pendulum.timezone("Asia/Seoul")
 
 default_args = {
 	"owner": "230608/woorek",
 	"depends_on_past": "False",
-	"start_date": datetime(2023, 6, 8),
+	"start_date": datetime(2023, 6, 8, tzinfo=KST),
 	"retries": 0
 }
 
 
 #DAG
-dag = DAG('currency-dag', default_args=default_args, schedule_interval='@once')
+dag = DAG('get-currency-dag', default_args=default_args, schedule_interval='@once')
 
 # define functions to use
-def get_currency(date):
+def get_currency(date: str):
 	import FinanceDataReader as fdr
 
 	df = fdr.DataReader('USD/KRW', start=date)
 	print(df)
 
 # define variables to use
-date_used = datetime.now().strftime("%Y-%m-%d")
+param_date = "{{ next_execution_date.strftime('%Y-%m-%d') }}"
 
 # task Operators
 t1 = PythonOperator(
 	task_id = 'task_1',
-	provide_context = True,
+	# provide_context = True,
 	python_callable = get_currency,
 	op_kwargs = {"date" : date_used},
 	dag = dag
