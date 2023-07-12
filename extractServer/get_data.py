9ea8b5e5f44246421891c1591e09b0a23789de05
J3llyBe4n <zza0423@gmail.com>
2023-07-12 15:11:01 +0900

    추가 operator에 사용할 API 요청 구현

--- extractServer/get_data.py
+++ extractServer/get_data.py
@@ -320,6 +320,7 @@ async def get_dart_code():
     file_path = os.path.join(directory, "dart_code.csv")
     tmp_file.to_csv(path_or_buf=file_path, index=False)
 
+# dart 공시코드 db 밀어넣는 코드
 @app.get("/dartcode/db/all")
 async def update_dart_code():
 
@@ -349,6 +350,7 @@ async def update_dart_code():
 
     conn.close()
 
+# 기존 hdfs 밀어넣는 코드
 @app.get("/hdfs/{data_category}/{exe_day}")
 async def upload_to_hdfs(data_category:str, exe_day:str):
     import subprocess
@@ -374,6 +376,7 @@ async def upload_to_hdfs(data_category:str, exe_day:str):
 
         result = subprocess.run(command, shell=True, capture_output=True, text=True)
 
+# 기존 hdfs 데이터 체크 코드
 @app.get("/check/hdfs/{data_category}/{exe_day}")
 async def check_done_flag(data_category:str, exe_day:str):
     import subprocess
@@ -400,17 +403,17 @@ async def check_done_flag(data_category:str, exe_day:str):
         else:
             return "1"
 
-@app.get("/loadHDFS/{exe_day}")
-async def download_from_hdfs(exe_day:str):
-
-@app.get("/checkHDFS-folder")
+# 신 hdfs 데이터 정합성 체크
+@app.get("/checkHDFS-folder/data_category={data_category}/exe_year={exe_year}/exe_period={exe_period}")
 async def check_folder(data_category: str, exe_year: str, exe_period: str):
+    import sys
     from pywebhdfs.webhdfs import PyWebHdfsClient
     hdfs = PyWebHdfsClient(host='192.168.90.128', port='9870', user_name='yoda')
     base_path = '/user/stock/raw/price_data'
 
-    if data_category in ['KOSPI/day', 'KOSDAQ/day'] or data_category in ['KOSPI/minute', 'KOSDAQ/minute']:
-        folder_path = f"{base_path}/{data_category}/{exe_year}/{exe_period}"
+    if data_category in ['KOSPI-day', 'KOSDAQ-day'] or data_category in ['KOSPI-minute', 'KOSDAQ-minute']:
+        market_name, duration = data_category.split('-')
+        folder_path = f"{base_path}/{market_name}/{duration}/{exe_year}/{exe_period}"
         response = None
         try:
             response = hdfs.list_dir(folder_path)
@@ -418,10 +421,175 @@ async def check_folder(data_category: str, exe_year: str, exe_period: str):
             pass
 
         if response is None:
-            return {"status": "not exist"}
+            return "1"
         else:
             return {"status": "success"}
     else:
-        return {"status": "invalid data category"}
+        return "1"
 
-##    
+# Warehouse 필요 데이터 다운로드
+@app.get("/from/hdfs/data_category={data_category}/exe_day={exe_day}")
+async def download_from_hdfs(data_category: str, exe_day: str):
+    import subprocess
+
+    data_category = data_category.upper()
+    hdfs_base_path = "/user/stock/raw"
+    local_base_path = "/home/yoda/stock/raw"
+
+    year, month, day = exe_day.split('-')
+
+    if data_category in ['KOSPI-DAY', 'KOSDAQ-DAY']:
+        category = data_category.split('-')[0]
+        duration = data_category.split('-')[1].lower()
+
+        if month in ['01', '02', '03']:
+            path_suffix = "1"
+        elif month in ['04', '05', '06']:
+            path_suffix = "2"
+        elif month in ['07', '08', '09']:
+            path_suffix = "3"
+        elif month in ['10', '11', '12']:
+            path_suffix = "4"
+        else:
+            return "Invalid month"
+
+        additional_path = f"/price_data/{category}/{duration}/{year}"
+        local_path = f"{local_base_path}{additional_path}/{path_suffix}"
+        hdfs_path = f"{hdfs_base_path}{additional_path}/{path_suffix}"
+
+        command1 = f"mkdir -p {local_path}"
+        subprocess.run(command1, shell=True, capture_output=True, text=True)
+
+        command2 = f"hdfs dfs -copyToLocal {hdfs_path}/* {local_path}"
+        result = subprocess.run(command2, shell=True, capture_output=True, text=True)
+
+    elif data_category in ['KOSPI-MINUTE', 'KOSDAQ-MINUTE']:
+        category = data_category.split('-')[0]
+        duration = data_category.split('-')[1].lower()
+
+        additional_path = f"/price_data/{category}/{duration}/{year}"
+        local_path = f"{local_base_path}{additional_path}/{month}"
+        hdfs_path = f"{hdfs_base_path}{additional_path}/{month}"
+
+        command1 = f"mkdir -p {local_path}"
+        subprocess.run(command1, shell=True, capture_output=True, text=True)
+
+        command2 = f"hdfs dfs -copyToLocal {hdfs_path}/* {local_path}"
+        result = subprocess.run(command2, shell=True, capture_output=True, text=True)
+    else:
+        return "Invalid data category"
+
+
+
+
+
+
+# 금일 분봉 신규 데이터와, warehouse 데이터 병합
+@app.get("concat-min-data/data_category={data_category}/exe_day={exe_day}")
+async def concat_min_data(data_category:str, exe_day:str):
+    year, month, day = exe_day.split('-')
+    start_date = datetime.strptime(exe_day, "%Y-%m-%d")
+    next_date = (start_date + timedelta(days=1)).strftime("%Y-%m-%d")
+
+    local_base_path = "/home/yoda/stock"
+    market_name , duration = data_category.split('-')
+    duration = duration.lower()
+    additional_path = f"/price_data/{market_name}/{duration}/{year}/{month}{day}"
+
+    import mysql.connector
+
+    conn = mysql.connector.connect(user='stock', password='1234', host='192.168.90.128', database='stock', port='3306', auth_plugin='mysql_native_password')
+    cursor = conn.cursor()
+    query = 'select company_code from {}_kode'.format(market_name.lower())
+    cursor.execute(query)
+    result = cursor.fetchall()
+    conn.close()
+
+    ticker_list = [row[0] for row in result]
+
+    for num in range(len(ticker_list)):
+        ticker_no = ticker_list[num]
+        info_num = ticker_no.split('.')[0]
+
+        local_data_path = f"{local_base_path}{additional_path}/{info_num}.csv"
+        copied_hdfs_data_path = f"/home/yoda/stock/raw/price_data/{market_name}/{duration}/{year}/{month}/{info_num}.csv"
+        
+        local_data = pd.read_csv(local_data_path)
+        hdfs_data = pd.read_csv(copied_hdfs_data_path)
+        combined_df = pd.concat([hdfs_data, local_data], ignore_index=True)
+        combined_df.to_csv(copied_hdfs_data_path)
+
+    done_file = f"/home/yoda/stock/raw/price_data/{market_name}/{duration}/{year}/{month}/DONE"
+    open(done_file, "w").close()
+
+# 금일 일봉 신규 데이터와, warehouse 데이터 병합
+@app.get("/concat-day-data/data_category={data_category}/exe_day={exe_day}")
+async def concat_day_data(data_category:str, exe_day:str):
+    year, month, day = exe_day.split('-')
+    start_date = datetime.strptime(exe_day, "%Y-%m-%d")
+    next_date = (start_date + timedelta(days=1)).strftime("%Y-%m-%d")
+
+    local_base_path = "/home/yoda/stock"
+    market_name , duration = data_category.split('-')
+    duration = duration.lower()
+    additional_path = f"/price_data/{market_name}/{duration}/{year}/{month}{day}"
+
+    import mysql.connector
+
+    conn = mysql.connector.connect(user='stock', password='1234', host='192.168.90.128', database='stock', port='3306', auth_plugin='mysql_native_password')
+    cursor = conn.cursor()
+    query = 'select company_code from {}_kode'.format(market_name.lower())
+    cursor.execute(query)
+    result = cursor.fetchall()
+    conn.close()
+
+    ticker_list = [row[0] for row in result]
+
+    if month in ['01', '02', '03']:
+        path_suffix = "1"
+    elif month in ['04', '05', '06']:
+        path_suffix = "2"
+    elif month in ['07', '08', '09']:
+        path_suffix = "3"
+    elif month in ['10', '11', '12']:
+        path_suffix = "4"
+    else:
+        return "Invalid month"
+
+    for num in range(len(ticker_list)):
+        ticker_no = ticker_list[num]
+        info_num = ticker_no.split('.')[0]
+
+        local_data_path = f"{local_base_path}{additional_path}/{info_num}.csv"
+        copied_hdfs_data_path = f"/home/yoda/stock/raw/price_data/{market_name}/{duration}/{year}/{path_suffix}/{info_num}.csv"
+        
+        local_data = pd.read_csv(local_data_path)
+        hdfs_data = pd.read_csv(copied_hdfs_data_path)
+        combined_df = pd.concat([hdfs_data, local_data], ignore_index=True)
+        combined_df.to_csv(copied_hdfs_data_path)
+
+    done_file = f"/home/yoda/stock/raw/price_data/{market_name}/{duration}/{year}/{month}/DONE"
+    open(done_file, "w").close()
+
+# 월초 신규 데이터 처리 하기 위한 crud
+@app.get("local_path_to_hdfs_path/data_category={data_category}/exe_day={exe_day}")
+async def local_path_to_hdfs_path(data_category:str, exe_day:str):
+    import subprocess
+
+    year, month, day = exe.day.split('-')
+    start_date = datetime.strptime(exe_day, "%Y-%m-%d")
+    next_date = (start_date + timedelta(days=1)).strftime("%Y-%m-%d")
+
+    market_name , duration = data_category.split('-')
+    duration = duration.lower()
+
+    local_base_path = "/home/yoda/stock"
+    additional_path = f"/price_data/{market_name}/{duration}/{year}/{month}{day}"
+    local_data_path = f"{local_base_path}{additional_path}"
+
+    copied_hdfs_base_path = f"/home/yoda/stock/raw/price_data/{market_name}/{duration}/{year}/{month}"
+
+    command = f"cp -r {local_data_path} {copied_hdfs_base_path}"
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+
+
